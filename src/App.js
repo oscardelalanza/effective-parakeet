d6f73afa6b52553bcecb647d50c2fc1d07ee9a2b
oscardelalanza <oscardelalanza@gmail.com>
2022-02-20 01:19:06 -0800

    formik general validations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import { useFormik } from 'formik'
 
+const validate = values => {
+  const errors = {}
+
+  if (!values.name) {
+    errors.name = 'Required'
+  } else if (values.name.length < 5) {
+    errors.name = 'Name too short'
+  }
+
+  if (!values.lastName) {
+    errors.lastName = 'Required'
+  } else if (values.lastName.length < 5) {
+    errors.name = 'lastName too short'
+  }
+
+
+  return errors
+}
+
 const App = () => {
   const formik = useFormik({
     initialValues: {
@@ -7,6 +26,7 @@ const App = () => {
       lastName: '',
       email: ''
     },
+    validate,
     onSubmit: values => console.log(values)
   })
 
@@ -14,8 +34,12 @@ const App = () => {
     <form onSubmit={formik.handleSubmit}>
       <label>Name</label>
       <input name="name" type="text" onChange={formik.handleChange} value={formik.values.name} />
+      {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+      <br/>
       <label>Last Name</label>
       <input name="lastName" type="text" onChange={formik.handleChange} value={formik.values.lastName} />
+      {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+      <br/>
       <label>Email</label>
       <input name="email" type="email" onChange={formik.handleChange} value={formik.values.email} />
       <button type="submit">Sent</button>
